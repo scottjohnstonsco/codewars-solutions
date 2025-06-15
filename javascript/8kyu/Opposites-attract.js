682dc8c5262a82e95e7b0607e04300760b6f2b67
Scott Johnston <209605268+scottjohnstonsco@users.noreply.github.com>
2025-06-15 12:49:47 +0900

    solution made with only hints this time

---
+++ javascript/8kyu/Opposites-attract.js
@@ -0,0 +1,18 @@
+//My solution 
+// https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
+
+
+
+function lovefunc(flower1, flower2){
+    if((flower1 % 2 == 0 || flower2 % 2 == 0) && (flower1 % 2 != 0 || flower2 % 2 != 0))
+      return true;
+    else return false;
+  }
+
+
+  /*   Other people solutions
+  function lovefunc(flower1, flower2){
+  return (flower1 + flower2) % 2 == 1;
+}
+*/
+
