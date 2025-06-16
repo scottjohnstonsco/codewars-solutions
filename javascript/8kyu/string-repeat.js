07f38ad9927e4671cf1dd8214b0071ef138adb93
Scott Johnston <209605268+scottjohnstonsco@users.noreply.github.com>
2025-06-16 10:39:08 +0900

    string repeat solution

---
+++ javascript/8kyu/string-repeat.js
@@ -0,0 +1,14 @@
+/* String repeat
+https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/train/javascript
+My solution using template literal
+*/
+
+function repeatStr (n, s) {
+    return `${s.repeat(n)}`;
+  }
+
+  /* Other solutions
+function repeatStr (n, s) {
+  return s.repeat(n);
+}
+  */
